d893315b48bef46d472d552fdf63cda4f545defc
Nicole Bekkers <hackybekkers@hacklor.com>
2019-09-11 15:01:58 +0200

    Show if question was answered correctly

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
 
 class Results extends Component {
+  constructor(props) {
+    super(props)
+
+    this.question = this.props.question
+  }
+
+  answeredCorrectly() {
+    return this.question.answer === this.question.userAnswer
+  }
+
   render() {
+    const text = this.answeredCorrectly() ? 'Correct!' : 'Incorrect'
     return (
-      <div>Results</div>
+      <div>{text}</div>
     )
   }
 }
