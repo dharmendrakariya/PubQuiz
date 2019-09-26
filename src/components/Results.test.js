8a4165cc5374b2dbab2453d7181e938d5c5e0e38
Nicole Bekkers <hackybekkers@hacklor.com>
2019-09-26 22:19:06 +0200

    Shows results for multiple questions

--- src/components/Results.test.js
+++ src/components/Results.test.js
@@ -3,22 +3,46 @@ import { render } from '@testing-library/react';
 import Results from './Results';
 
 describe(Results, () => {
-  const question = {
+  const questions = [{
+    id: 'unique_id_1',
     text: 'What is the answer to life, the universe and everything?',
-    answer: '42'
-  }
+    choices: ['40', '41', '42', '43'],
+    answer: '42',
+    playerAnswer: null,
+    timeLimitInSeconds: 10
+  }, {
+    id: 'unique_id_2',
+    text: 'What is the name of the Colour of Magic?',
+    choices: ['Octopus', 'Octarine', 'Ocarina', 'Octagon'],
+    answer: 'Octarine',
+    playerAnswer: null,
+    timeLimitInSeconds: 5
+  }]
 
-  it('shows that the question was answered correctly', () => {
-    question.userAnswer = '42'
+  it('shows that all questions were answered correctly', () => {
+    questions[0].userAnswer = '42'
+    questions[1].userAnswer = 'Octarine'
 
-    const { queryByText } = render(<Results question={question} />)
-    expect(queryByText('Correct!')).not.toBeNull()
+    const { queryByText } = render(<Results questions={questions} />)
+    expect(queryByText('Question 0: Correct!')).not.toBeNull()
+    expect(queryByText('Question 1: Correct!')).not.toBeNull()
   })
 
-  it('shows that the question was answered incorrectly', () => {
-    question.userAnswer = '43'
+  it('shows that one of the questions was answered correctly', () => {
+    questions[0].userAnswer = '43'
+    questions[1].userAnswer = 'Octarine'
 
-    const { queryByText } = render(<Results question={question} />)
-    expect(queryByText('Incorrect')).not.toBeNull()
+    const { queryByText } = render(<Results questions={questions} />)
+    expect(queryByText('Question 0: Incorrect')).not.toBeNull()
+    expect(queryByText('Question 1: Correct!')).not.toBeNull()
+  })
+
+  it('shows that all questions were answered incorrectly', () => {
+    questions[0].userAnswer = '43'
+    questions[1].userAnswer = 'Octarina'
+
+    const { queryByText } = render(<Results questions={questions} />)
+    expect(queryByText('Question 0: Incorrect')).not.toBeNull()
+    expect(queryByText('Question 1: Incorrect')).not.toBeNull()
   })
 })
