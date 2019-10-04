5fb16709758de46b61e5beccee2211d4ac378b4e
Nicole Bekkers <hackybekkers@hacklor.com>
2019-10-04 14:57:15 +0200

    Move questions to quizzes directory
    In preparation for multiple quizzes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import questions from './questions.json';
 import Quiz from './components/Quiz';
 import styles from './App.module.scss';
+import quiz from './quizzes/nerd-quiz.json';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      questions
+      quiz
     }
   }
 
@@ -19,7 +19,7 @@ export default class App extends React.Component {
           PubQuiz
         </header>
         <main className={styles.main}>
-          <Quiz questions={this.state.questions}/>
+          <Quiz questions={this.state.quiz}/>
         </main>
       </div>
     )
