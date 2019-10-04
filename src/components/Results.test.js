af1a207ff8a3dc1e267660938902f21c0e8b7eef
Nicole Bekkers <hackybekkers@hacklor.com>
2019-10-04 17:46:38 +0200

    Remove default for playerAnswer in JSON

--- src/components/Results.test.js
+++ src/components/Results.test.js
@@ -8,16 +8,20 @@ describe(Results, () => {
     text: 'What is the answer to life, the universe and everything?',
     choices: ['40', '41', '42', '43'],
     answer: '42',
-    playerAnswer: null,
     timeLimitInSeconds: 10
   }, {
     id: 'unique_id_2',
     text: 'What is the name of the Colour of Magic?',
     choices: ['Octopus', 'Octarine', 'Ocarina', 'Octagon'],
     answer: 'Octarine',
-    playerAnswer: null,
     timeLimitInSeconds: 5
   }]
+
+  it("shows incorrect when player hasn't answered", () => {
+    const { queryByText } = render(<Results questions={questions} />)
+    expect(queryByText('Question 0: Incorrect')).not.toBeNull()
+    expect(queryByText('Question 1: Incorrect')).not.toBeNull()
+  })
 
   it('shows that all questions were answered correctly', () => {
     questions[0].playerAnswer = '42'
