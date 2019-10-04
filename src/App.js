9cc9ab7634cea9c73e21bd9cff7d227994aa8b82
Nicole Bekkers <hackybekkers@hacklor.com>
2019-10-04 14:03:01 +0200

    Convert App CSS to module

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import questions from './questions.json';
-import './App.css';
 import Quiz from './components/Quiz';
+import styles from './App.module.css';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -14,11 +14,11 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className={styles.app}>
+        <header className={styles.header}>
           PubQuiz
         </header>
-        <main className="App-main">
+        <main className={styles.main}>
           <Quiz questions={this.state.questions}/>
         </main>
       </div>
